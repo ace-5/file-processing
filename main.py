2b4414e37fb38d03c754ea1ed7a7efe6b1466303
ashmin <ashminniraula@gmail.com>
2022-06-29 08:20:48 +0545

    Update: Output 20 most frequent combination  of each ngram(1, 2, 3)

--- main.py
+++ main.py
@@ -2,15 +2,29 @@ from packages import utils
 
 notes = utils.read_csv('govt_urls_state_only.csv', 'Note')
 
-write_to_file =[]
+unigram =[]
+bigram =[]
+trigram =[]
+json_dict = {
+    "Unigram": {},
+    "Bigram": {},
+    "Trigram": {}
+    }
+
+def make_json(key, ngram, n=20):
+    for k,v in utils.n_gram_freq(ngram, n):
+        json_dict[key][k] = v
 
 for lines in notes:
-    removed_punctuation = utils.remove_punctuation(lines)
-    unigram = (utils.generate_n_grams(removed_punctuation, 1))
-    bigram = (utils.generate_n_grams(removed_punctuation, 2))
-    trigram = (utils.generate_n_grams(removed_punctuation, 3))
-    write_to_file.append(utils.ngram_dict(lines, unigram, bigram, trigram))
+    clean = utils.remove_punctuation(lines)
+    text = clean.split()
+    unigram.extend(utils.generate_n_grams(text[:-4], 1))
+    bigram.extend(utils.generate_n_grams(text[:-4], 2))
+    trigram.extend(utils.generate_n_grams(text[:-4], 3))
 
-columns = write_to_file[0].keys()
+make_json('Unigram', unigram)
+make_json('Bigram', bigram)
+make_json('Trigram', trigram)
 
-utils.write_csv(write_to_file, columns, 'result.csv')
+
+utils.write_json('result.json', json_dict)
--- packages/utils.py
+++ packages/utils.py
@@ -1,6 +1,8 @@
 import csv
 from nltk.corpus import stopwords
 import string
+import json
+from collections import Counter
 
 en_stopwords = stopwords.words('english')
 
@@ -18,27 +20,41 @@ def remove_punctuation(line):
         if letter in string.punctuation:
             continue
         returnline += letter
-    
-    return returnline
+    lower = returnline.lower()
+    return lower
 
 def generate_n_grams(text, n_gram):
     n_gram_result = []
-    words = text.split()
-    words = [word for word in words if word.lower() not in en_stopwords]
+    words = [word for word in text if word.lower() not in en_stopwords]
     for i in range(len(words)-n_gram+1):
             n_gram_result.append(words[i:i+n_gram])
     return n_gram_result
 
-def ngram_dict(line, unigram, bigram, trigram):
-    return({
-        "Raw": line,
-        "Unigram": unigram,
-        "Bigram": bigram,
-        "Trigram": trigram
-    })
+
+def n_gram_freq(ngram, n):
+    """
+    Returns first n most frequent value of ngram along with it's frequency
+    """
+    frequency = {}
+    for items in ngram:
+        if f'{items}' not in frequency.keys():
+            frequency[f'{items}'] = 1
+        else:
+            frequency[f'{items}'] += 1
+
+    return (list(sorted(frequency.items(), key =
+                lambda kv:(kv[1], kv[0]), reverse= True))[:n])
+            
+
 
 def write_csv(to_write, columns, filename):
     with open (filename, 'w') as f:
         writer = csv.DictWriter(f, fieldnames=columns)
         writer.writeheader()
         writer.writerows(to_write)
+
+
+def write_json(filename, json_dict):
+    with open (filename, 'w') as f:
+        writer = json.dumps(json_dict, indent=2, ensure_ascii = False)
+        f.write(writer)
