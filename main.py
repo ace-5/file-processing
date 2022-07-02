f6d909819a86d52e6f0d5d3cbb4bf5a663f6c8eb
ashmin <ashminniraula@gmail.com>
2022-07-02 12:30:44 +0545

    correct blunder

--- main.py
+++ main.py
@@ -3,20 +3,27 @@ from packages import utils
 notes = utils.read_csv('govt_urls_state_only.csv', 'Note')
 
 line_wise_ngram = []
+all_n_gram = []
 n_grams_with_freq = []
+n_gram_string = []
+all_clean_lines = []
 write_this = []
 top_20_ngram = []
-# loop to collect ngarms line wise
-for i in range(len(notes)):
-    clean = utils.remove_punctuation(notes[i])
+
+count = 0
+for lines in notes:
+    words = []
+    temp = []
+    clean = utils.remove_punctuation(lines)
     text = clean.split('--')[0].split()
     line = [word for word in text if word not in utils.en_stopwords]
-    line_str = ' '.join(line)
-    line_wise_ngram.append(utils.generate_n_grams(line_str, 1))
-    line_wise_ngram[i].extend(utils.generate_n_grams(line_str, 2))
-    line_wise_ngram[i].extend(utils.generate_n_grams(line_str, 3))
+    temp.append(' '.join(line))
+    line_wise_ngram.append(utils.generate_n_grams(temp, 1))
+    line_wise_ngram[count].extend(utils.generate_n_grams(temp, 2))
+    line_wise_ngram[count].extend(utils.generate_n_grams(temp, 3))
+    all_clean_lines.append(' '.join(line))
+    count+=1
 
-# append all ngrams in specific line to common list for that line
 ngrams_str_linewise = []
 for i in range(len(line_wise_ngram)):
     x = []
@@ -24,15 +31,20 @@ for i in range(len(line_wise_ngram)):
         x.append(" ".join(items))
     ngrams_str_linewise.append(x)
 
-# collect top 20 of each ngrams to a common list
+
+for i in range(3):
+    temp = []
+    all_n_gram.append(utils.generate_n_grams(all_clean_lines, i+1))
+    for items in all_n_gram[i]:
+        temp.append(" ".join(items))
+    n_gram_string.append(temp) 
+    n_grams_with_freq.append(utils.n_gram_freq(n_gram_string[i]))
+
+
 top_20_ngram.extend(ngram for ngram, freq in n_grams_with_freq[0][:20])
 top_20_ngram.extend(ngram for ngram, freq in n_grams_with_freq[1][:20])
 top_20_ngram.extend(ngram for ngram, freq in n_grams_with_freq[2][:20])
 
-# compare if ngrams of current line is in top n-gram
-# When True: join all unique ngrams of that line, which are present in top 20, together as string
-# append to list which contains dict as it's item
-# each dict contains original line and it's ngram in top n gram 
 for i in range(len(notes)):
     current_ngram = []
     for items in ngrams_str_linewise[i]:
@@ -42,5 +54,5 @@ for i in range(len(notes)):
         'ngrams': ",".join(set(current_ngram)),
         "Notes": notes[i]
     })
-
+    
 utils.write_csv('notes_with_ngrams.csv', write_this, write_this[0].keys())
